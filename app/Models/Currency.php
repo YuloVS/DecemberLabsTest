34e64a8e961650ccbbbedcc808297825468cea43
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-14 19:21:20 -0300

    Update relations

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,10 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Currency extends Model
 {
     protected $fillable = [
         "symbol", "name"
     ];
+
+    public function account()
+    : BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
 }
