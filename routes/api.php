d222816b8b968a0ce2ba7acef6810c95c7eb7a98
Giuliano Vallejos Santajuliana <63474125+YuloVS@users.noreply.github.com>
2021-05-16 17:57:13 -0300

    Add API routes to account

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\V1\AccountController;
 use App\Http\Controllers\Api\V1\LoginController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::post('v1/login', [LoginController::class, 'login']);
+Route::post('v1/login', [LoginController::class, 'login'])->middleware("guest:sanctum");
+
+Route::apiResource('v1/accounts', AccountController::class)
+    ->only(["index", "show"])
+    ->middleware("auth:sanctum");
